d57f3a8542ac69d3f7e7b14b27d20b16f2ee8640
Samuel Njuguna <41824317+mydax-kid@users.noreply.github.com>
2023-03-22 02:29:09 +0000

    Initial commit

---
+++ README.txt
@@ -0,0 +1,13 @@
+//create a new package.json file to track our dependencies
+  npm init-y 
+
+//install webpack and webpack CLI
+  npm i webpack webpack-cli -D
+
+//create a new webpack config file in root dir
+  touch webpack.config.js
+  populate the file with necessary data
+  create a webpack script on package.json
+
+//install firebase
+  npm i firebase
---
+++ dist/bundle.js
@@ -0,0 +1,49 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./index.js":
+/*!******************!*\
+  !*** ./index.js ***!
+  \******************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\nObject(function webpackMissingModule() { var e = new Error(\"Cannot find module 'firebase/app'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }());\n\n\nconst firebaseConfig = {\n  apiKey: \"AIzaSyDmXgb_58lO7aK_ujN37pGlNxzWGEU0YpI\",\n  authDomain: \"fb9-sandbox.firebaseapp.com\",\n  projectId: \"fb9-sandbox\",\n  storageBucket: \"fb9-sandbox.appspot.com\",\n  messagingSenderId: \"867529587246\",\n  appId: \"1:867529587246:web:dc754ab7840c737f47bdbf\"\n}\n\n// init firebase\nObject(function webpackMissingModule() { var e = new Error(\"Cannot find module 'firebase/app'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }())(firebaseConfig)\n\n//# sourceURL=webpack://nodejs/./index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The require scope
+/******/ 	var __webpack_require__ = {};
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./index.js"](0, __webpack_exports__, __webpack_require__);
+/******/ 	
+/******/ })()
+;
